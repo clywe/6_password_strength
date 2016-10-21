55b17ef433f257e17a3e476db2b8a9335f212bd9
clywe <cheredima@ya.ru>
2016-10-21 21:26:04 +0300

    Учёл pep8

--- password_strength.py
+++ password_strength.py
@@ -2,22 +2,22 @@ import re
 
 
 def get_password_strength(password):
-    digits = re.findall(r'\d',password)
-    if len(digits) == len(password): #пароль состоит только из чисел?
+    digits = re.findall(r'\d', password)
+    if len(digits) == len(password):  # пароль состоит только из чисел?
         return 1
-    letters = re.findall(r'\w\D',password)
+    letters = re.findall(r'\w\D', password)
     upper_letters = 0
     for letter in letters:
         if letter.isupper():
             upper_letters += 1
-    if len(letters) == len(password):#пароль состоит только из букв?
+    if len(letters) == len(password):  # пароль состоит только из букв?
         return 1 if upper_letters == 0 else 2
     strength = 2
     lower_letters = len(letters) - upper_letters
     strength += oneormoretimes(upper_letters)
     strength += oneormoretimes(lower_letters)
     strength += oneormoretimes(len(digits))
-    symbols = re.findall(r'\W',password)
+    symbols = re.findall(r'\W', password)
     strength += oneormoretimes(len(symbols))
     return strength
 
@@ -27,4 +27,4 @@ def oneormoretimes(digit):
 
 
 if __name__ == '__main__':
-    print (get_password_strength("123a"))
+    print(get_password_strength("Password1"))
